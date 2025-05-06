4389cb485626686e9e4ecd9693d548bcc57a86c8
Chris <christopherkaelin04@gmail.com>
2025-05-05 22:03:06 -0400

    Begun Feature Extraction for Phase 1

--- src/bovw.py
+++
@@ -1,6 +0,0 @@
-#Code by Christopher Kaelin, 2025
-# This code is licensed under the MIT License - see the LICENSE file for details.
-'''
-This file provides a basic implementation of the Bag of Visual Words (BoVW) model for image classification.
-It includes functions for creating a visual vocabulary, extracting features from images, and classifying images using the BoVW model.
-'''
